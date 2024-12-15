48e66da4ac342afa027a901970cdcd739dedc543
Shinjan P <116586681+flaminshinjan@users.noreply.github.com>
2024-12-15 09:48:37 +0530

    Initial Commit

---
+++ moonify.ai/moonify_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  moonify_aiApp.swift
+//  moonify.ai
+//
+//  Created by Shinjan Patra on 15/12/24.
+//
+
+import SwiftUI
+
+@main
+struct moonify_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ moonify.aiTests/moonify_aiTests.swift
@@ -0,0 +1,17 @@
+//
+//  moonify_aiTests.swift
+//  moonify.aiTests
+//
+//  Created by Shinjan Patra on 15/12/24.
+//
+
+import Testing
+@testable import moonify_ai
+
+struct moonify_aiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
